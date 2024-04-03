7e8a7c126ca865078bcfec94c3079e39fd8c14fd
guido gutierrez <121067148+guidoo25@users.noreply.github.com>
2024-04-02 19:31:32 -0500

    cambioget

--- routes/product_get.js
+++ routes/product_get.js
@@ -9,40 +9,39 @@ router.get('/productos', async (req, res) => {
         const limit = 35;
         const offset = (page - 1) * limit;
 
-        const { rows } = await pool.query(`
-            SELECT p.id, p.nombre, p.lugar, p.imagen AS photo, p.product_url AS urlproducto, h.precio, h.fecha, COUNT(h.producto_id) AS historial_count
-            FROM productos p
-            JOIN (
-                SELECT producto_id, precio, fecha
+            const { rows } = await pool.query(`
+        SELECT p.id, p.nombre, p.lugar, p.imagen AS photo, p.product_url AS urlproducto, h.precio, h.fecha
+        FROM productos p
+        JOIN (
+            SELECT producto_id, precio, fecha
+            FROM historial_precios
+            WHERE (producto_id, fecha) IN (
+                SELECT producto_id, MAX(fecha)
                 FROM historial_precios
-                WHERE (producto_id, fecha) IN (
-                    SELECT producto_id, MAX(fecha)
-                    FROM historial_precios
-                    GROUP BY producto_id
-                )
-            ) h ON p.id = h.producto_id
-            GROUP BY p.id, h.precio, h.fecha
-            ORDER BY historial_count DESC, RANDOM()
-            LIMIT $1 OFFSET $2
-        `, [limit, offset]);
+                GROUP BY producto_id
+            )
+        ) h ON p.id = h.producto_id
+        ORDER BY RANDOM()
+        LIMIT $1 OFFSET $2
+    `, [limit, offset]);
 
-        // Opcional: calcular el total de páginas
-        const totalRes = await pool.query(`
-            SELECT COUNT(*) FROM productos;
-        `);
-        const totalRows = totalRes.rows[0].count;
-        const totalPages = Math.ceil(totalRows / limit);
+    // Opcional: calcular el total de páginas
+    const totalRes = await pool.query(`
+        SELECT COUNT(*) FROM productos;
+    `);
+    const totalRows = totalRes.rows[0].count;
+    const totalPages = Math.ceil(totalRows / limit);
 
-        res.json({
-            data: rows,
-            totalPages: totalPages,
-            currentPage: page,
-            limit: limit
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error al obtener los productos');
-    }
+    res.json({
+        data: rows,
+        totalPages: totalPages,
+        currentPage: page,
+        limit: limit
+    });
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Error al obtener los productos');
+}
 });
 
 router.get('/productos/categoria/:categoria', async (req, res) => {
@@ -80,7 +79,7 @@ router.get('/productos/mayor-baja', async (req, res) => {
             JOIN historial_precios h2 ON p.id = h2.producto_id
             WHERE h1.fecha < h2.fecha
             ORDER BY RANDOM()
-            LIMIT 12
+            LIMIT 20
         `);
 
         res.json(rows);
